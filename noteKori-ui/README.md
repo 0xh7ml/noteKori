f2b5a2539b9c09b75bc1cd95cdbaf8abfcc03873
Md Saikat <0xh7ml.py@gmai.com>
2023-08-05 23:46:14 +0600

    noteKori ui update

--- noteKori-ui/README.md
+++ noteKori-ui/README.md
@@ -1,1 +1,12 @@
 # NoteKori UI
+
+## ToDo
+- [x] index
+- [x] home
+- [x] login
+- [x] register
+- [ ] expense list
+- [ ] deposit list
+- [ ] add expense
+- [ ] add deposit
+- [ ] edit profile
---
+++ noteKori-ui/asset/js/main.js
@@ -0,0 +1,5 @@
+// index 
+const newLocation = 'http://127.0.0.1:5500/noteKori-ui/'
+document.getElementById('get-started').addEventListener('click', ()=>{
+    location.replace(newLocation+'login.html');
+})
